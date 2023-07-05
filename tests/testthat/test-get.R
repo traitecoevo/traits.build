d1e3d72eff2aa19afb9837d1f989cb828034b324
yangsophieee <sophie.yang917@gmail.com>
2023-07-05 11:16:14 +1000

    Change some tests to `expect_type`

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -4,7 +4,7 @@ test_that("test get_schema",{
   expect_type(x, "list")
   expect_length(x, 6)
   expect_equal(names(x), c("entity_type", "value_type", "basis_of_value", "basis_of_record", "austraits", "metadata"))
-  expect_class(get_schema(subsection = "austraits"), "list")
+  expect_type(get_schema(subsection = "austraits"), "list")
   expect_length(get_schema(subsection = "austraits"), 3)
   expect_length(get_schema(subsection = "metadata"), 3)
   expect_length(get_schema(subsection = "value_type"), 3)
--- tests/testthat/test-process.R
+++ tests/testthat/test-process.R
@@ -1,16 +1,16 @@
 
-Test_data <- "data/Test_2022/data.csv"
+test_data <- "data/Test_2022/data.csv"
 
 schema <- get_schema()
 resource_metadata <- get_schema("config/metadata.yml", "metadata")
 traits_definitions <- get_schema("config/traits.yml", "traits")
 unit_conversions <- get_unit_conversions("config/unit_conversions.csv")
-Test_config <- dataset_configure("data/Test_2022/test-metadata.yml",
-                             traits_definitions,
-                             unit_conversions)
+test_config <- dataset_configure("data/Test_2022/test-metadata.yml",
+                                  traits_definitions,
+                                  unit_conversions)
 
 test_that("test dataset_configure is working", {
-  expect_class(dataset_configure("data/Test_2022/test-metadata.yml",
+  expect_type(dataset_configure("data/Test_2022/test-metadata.yml",
                 yaml::read_yaml("config/traits.yml"),
                 get_unit_conversions("config/unit_conversions.csv")), "list")
   expect_length(dataset_configure("data/Test_2022/test-metadata.yml",
@@ -24,20 +24,20 @@ test_that("test dataset_configure is working", {
 
 test_that("test dataset_process is working",{
   austraits_names <- schema$austraits$elements %>% names()
-  
-  expect_no_error(  x <- dataset_process(Test_data, Test_config, schema,resource_metadata))
+
+  expect_no_error(  x <- dataset_process(test_data, test_config, schema,resource_metadata))
   expect_class(x, "list")
   expect_length(x, 13)
   expect_named(x, austraits_names)
   expect_equal(nrow(x$excluded_data), 0)
-  expect_equal(nrow(dataset_process(Test_data, Test_config, schema, resource_metadata, filter_missing_values = TRUE)$excluded_data), 0)
-  expect_equal(nrow(dataset_process(Test_data, Test_config, schema, resource_metadata, filter_missing_values = FALSE)$excluded_data), 44)
+  expect_equal(nrow(dataset_process(test_data, test_config, schema, resource_metadata, filter_missing_values = TRUE)$excluded_data), 0)
+  expect_equal(nrow(dataset_process(test_data, test_config, schema, resource_metadata, filter_missing_values = FALSE)$excluded_data), 44)
 })
 
 test_that("test process_custom_code is working",{
-  metadata <- Test_config$metadata
-  data <- readr::read_csv(Test_data, col_types = cols(), guess_max = 100000, progress=FALSE)
-  
+  metadata <- test_config$metadata
+  data <- readr::read_csv(test_data, col_types = cols(), guess_max = 100000, progress=FALSE)
+
   expect_equal(ncol(data), 13)
   expect_equal(ncol(process_custom_code(metadata[["dataset"]][["custom_R_code"]])(data)), 14)
   expect_silent(process_custom_code(NULL))
@@ -46,7 +46,7 @@ test_that("test process_custom_code is working",{
 # test_that("test process_flag_unsupported_traits is working",{
 #   process_flag_unsupported_traits(data, definitions)
 # })
-# 
+#
 # test_that("test process_flag_excluded_observations is working",{
 #   process_flag_excluded_observations
 # })
