4677ebe4b003e5cdab28b6865fd567ee338d5342
yangsophieee <sophie.yang917@gmail.com>
2023-07-12 17:03:06 +1000

    Review functions.R

--- tests/testthat/functions.R
+++ tests/testthat/functions.R
@@ -1,4 +1,7 @@
 
+# I think the functions aren't printing any error messages for some reason
+# Also are these functions supposed to be the same as those in testdata.R?
+
 # I think this function already exists in the package
 # Better than expect_silent as contains `info` and allows for complete failures
 expect_no_error <- function(object, regexp = NULL, ..., info = NULL, label = NULL) {
@@ -22,6 +25,7 @@ expect_unique <- function(object, info = NULL, label = NULL) {
   invisible(object)
 }
 
+# I think this already exists, with `testthat::expect_in`, should I remove and replace the others with this?
 expect_isin <- function(object, expected, ..., info = NULL, label = NULL,
                         expected.label = NULL, na.rm = TRUE) {
 
@@ -38,6 +42,7 @@ expect_isin <- function(object, expected, ..., info = NULL, label = NULL,
   invisible(object)
 }
 
+
 expect_not_NA <- function(object, info = NULL, label = NULL) {
   i <- !is.na(object)
   comp <- compare(all(i), TRUE)
@@ -46,10 +51,12 @@ expect_not_NA <- function(object, info = NULL, label = NULL) {
   invisible(object)
 }
 
+# This is easily replaced with expect_type right?
 test_list <- function(data, info) {
   expect_true(class(data) == "list", info = info)
 }
 
+# Should these function names follow the expect_ syntax?
 test_list_names_valid <- function(data, info) {
   test_list(data, info)
   expect_not_NA(names(data), info = info)
@@ -57,10 +64,12 @@ test_list_names_valid <- function(data, info) {
   expect_unique(names(data), info = info)
 }
 
+
 test_list_named <- function(data, expected_names, info) {
   test_list_names_valid(data, info)
   expect_named(data, expected_names, info = info)
 }
+
 
 test_list_named_contains <- function(data, expected_names, info) {
   test_list_names_valid(data, info)
@@ -75,6 +84,7 @@ test_dataframe_valid <- function(data, info) {
   expect_true(is.data.frame(data), info = info)
 }
 
+
 test_dataframe_named <- function(data, expected_colnames, info) {
   test_dataframe_valid(data, info)
   expect_named(data, expected_colnames, info = info)
@@ -84,7 +94,7 @@ test_dataframe_named <- function(data, expected_colnames, info) {
 test_build_dataset <- function(
   path_metadata, path_data, info, definitions, unit_conversions, schema, resource_metadata, taxon_list) {
 
-  # test it builds with no errors
+  # Test it builds with no errors
   expect_no_error({
     build_config <- dataset_configure(path_metadata, definitions, unit_conversions)
   }, info = paste(info, " config"))
@@ -102,6 +112,7 @@ test_build_dataset <- function(
   build_dataset
 }
 
+# This is not used in any code
 test_structure <- function(
   data, info, schema, definitions, single_dataset = TRUE) {
 
@@ -111,13 +122,15 @@ test_structure <- function(
   vars_tables <-
     vars_austraits %>%
     subset(., !(. %in% c("dataset_id", "definitions", "schema", "sources", "metadata", "build_info")))
+  # Getting a warning: no visible binding for global variable "."
+  # Can be fixed by adding utils::globalVariables(c(".")) somewhere in the package, maybe "R/globals.R"
 
-  # test lists have the right objects
+  # Test lists have the right objects
   comparison <- vars_austraits
 
   test_list_named(data, comparison, info = c(info, " - main elements"))
 
-  # test structure of tables
+  # Test structure of tables
   for (v in vars_tables) {
 
     comparison <- schema$austraits$elements[[v]]$elements %>% names()
@@ -125,6 +138,6 @@ test_structure <- function(
     test_dataframe_named(data[[v]], comparison, info = paste(info, " - structure of ", v))
   }
 
-  # contains allowed traits
+  # Contains allowed traits
   expect_isin(data$traits$trait_name %>% unique(), definitions$elements %>% names(), info = paste("traits ", v))
 }
