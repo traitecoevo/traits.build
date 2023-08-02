8700a5b7f2f3f7f51cb72389bd6eb007d59e40a7
yangsophieee <sophie.yang917@gmail.com>
2023-08-02 14:43:27 +1000

    Add testing for Test Dataset 2

--- tests/testthat/test-xamples-new.R
+++ tests/testthat/test-xamples-new.R
@@ -5,6 +5,7 @@ unit_conversions <- traits.build:::get_unit_conversions("config/unit_conversions
 taxon_list <- read_csv_char("config/taxon_list.csv")
 examples_dir <- "examples"
 
+
 testthat::test_that("Test Dataset 1 builds correctly", {
 
   # Test Dataset 1: Test_2023_1
@@ -45,6 +46,56 @@ testthat::test_that("Test Dataset 1 builds correctly", {
   # Check all tables are equal to expected output tables
   for (v in tables) {
     expect_equal(Test_2023_1[[v]], expected_output[[v]])
+  }
+
+})
+
+
+testthat::test_that("Test Dataset 2 builds correctly", {
+
+  # Test Dataset 2: Test_2023_2
+  # See README.md in examples/Test_2023_2 for details about this dataset
+
+  # Build dataset
+  expect_no_error(
+    Test_2023_2 <- test_build_dataset(
+      file.path(examples_dir, "Test_2023_2/metadata.yml"),
+      file.path(examples_dir, "Test_2023_2/data.csv"),
+      "Test Dataset 2", definitions, unit_conversions, schema, resource_metadata, taxon_list
+    ),
+    info = "Building Test Dataset 2")
+
+  # Expected output
+  tables <- c("traits", "locations", "contexts", "methods", "excluded_data",
+              "taxonomic_updates", "taxa", "contributors")
+  expect_no_error(
+    expected_output <-
+      purrr::map(
+        tables, ~read_csv(sprintf("examples/Test_2023_2/output/%s.csv", .x), col_types = "cccccccccccccccccccccccc")),
+    info = "Reading in expected output tables"
+  )
+  # Todo: also load and test non-csv outputs
+  names(expected_output) <- tables
+
+  # Temporary modifications to get these tests to pass
+  columns <- c("basis_of_value", "replicates", "life_stage", "collection_date", "measurement_remarks")
+
+  for (col in columns) {
+    names(expected_output[["traits"]][[col]]) <- rep("", length(expected_output[["traits"]][[col]]))
+  }
+
+  names(expected_output$locations$value) <- expected_output$locations$location_property
+  expected_output$methods <-
+    expected_output$methods %>%
+    mutate(across(c(source_secondary_key, source_original_dataset_key), ~""))
+
+  for (col in columns) {
+    names(expected_output[["excluded_data"]][[col]]) <- character(0)
+  }
+
+  # Check all tables are equal to expected output tables
+  for (v in tables) {
+    expect_equal(Test_2023_2[[v]], expected_output[[v]])
   }
 
 })
