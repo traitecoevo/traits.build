63925dfde63d5bf2b5413231762e02fa91ab4fc7
yangsophieee <sophie.yang917@gmail.com>
2023-08-09 12:27:03 +1000

    Add test for `metadata_add_locations`

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -102,6 +102,13 @@ test_that("metadata_add_source_bibtex is working", {
   expect_equal(read_metadata("data/Test_2022/metadata.yml")$source$primary$journal, "Journal of Ecology")
 })
 
+test_that("metadata_add_locations is working", {
+  locations <- read_csv("data/Test_2022/data.csv") %>%
+    select(site) %>% distinct() %>%
+    mutate(latitude = 1, longitude = 2, elevation = 100)
+  expect_no_error(metadata_add_locations(dataset_id = "Test_2022", locations))
+})
+
 test_that("metadata_add_substitution is working", {
   expect_silent(
     suppressMessages(
@@ -233,7 +240,7 @@ test_that("reports and plots are produced", {
   )
 })
 
-testthat::test_that("test_data is working", {
+testthat::test_that("dataset_test is working", {
   expect_silent(
     out <- dataset_test("Test_2022", reporter = testthat::SilentReporter))
   expect_in(
