53a671d3f861c0dacab058147dff2863963e279a
yangsophieee <sophie.yang917@gmail.com>
2023-08-23 13:49:16 +1000

    Update comments and documentations for utils

--- R/utils.R
+++ R/utils.R
@@ -25,12 +25,12 @@ NULL
 
 #' Read in a csv as a tibble with column types as characters
 #'
-#' Reads in a csv file using the read_csv function from readr
-#' with columns as characters
+#' Reads in a csv file using the `read_csv` function from readr
+#' with columns as characters.
 #'
-#' @param ... arguments passed to the read_csv()
+#' @param ... Arguments passed to the `read_csv()`
 #'
-#' @return tibble
+#' @return A tibble
 #' @export
 read_csv_char <- function(...) {
   readr::read_csv(..., col_types = cols(.default = "c"), progress = FALSE)
@@ -38,40 +38,40 @@ read_csv_char <- function(...) {
 
 #' Convert NULL values to a different value
 #'
-#' util_replace_null converts NULL values in a vector to a different value. Default is
-#' converting NuLL to NA
+#' `util_replace_null` converts NULL values a different value. Default is
+#' converting NULL to NA.
 #'
-#' @param x a vector containing null values
-#' @param val specify what the null value should be returned as, default is NA
+#' @param x A NULL value or a non-NULL object
+#' @param val Specify what the null value should be returned as, default is NA
 #'
-#' @return a vector with null values replaced
+#' @return NA or a non-NULL object
 #' @examples \dontrun{
 #' util_replace_null(NULL)
 #' }
 util_replace_null <- function(x, val = NA) {
   if (is.null(x)) return(val)
-  x
+    x
 }
 
 
 #' Convert all columns in data frame to character
 
-#' @param df a dataframe
+#' @param df A dataframe
 #'
-#' @return a dataframe
+#' @return A dataframe
 #'
-#' @examples lapply(traits.build:::util_df_convert_character(iris), class)
+#' @examples lapply(traits.build:::util_df_convert_character(dplyr::starwars), class)
 util_df_convert_character <- function(df) {
   dplyr::mutate(df, dplyr::across(dplyr::everything(), as.character))
 }
 
 #' Extract a trait element from the definitions$traits$elements
 #'
-#' @param i a value within the definitions$traits$elements list which refers to types of traits
-#' @param my_list the list that contains the element we're interested in (i.e. definitions$traits$elements)
-#' @param var the type of variable of a trait
+#' @param i A value within the definitions$traits$elements list which refers to types of traits
+#' @param my_list The list that contains the element we're interested in (i.e. definitions$traits$elements)
+#' @param var The type of variable of a trait
 #'
-#' @return the element/properties of a trait
+#' @return The element/properties of a trait
 #'
 #' @export
 #' @examples
@@ -86,20 +86,20 @@ util_extract_list_element <- function(i, my_list, var) {
 
 #'  Split and sort cells with multiple values
 #'
-#'  util_separate_and_sort: For a vector x in which individual cell may have
-#'  multiple values (separated by 'sep'), sort records within each cell alphabetically
+#'  `util_separate_and_sort`: For a vector x in which individual cell may have
+#'  multiple values (separated by 'sep'), sort records within each cell alphabetically.
 #'
-#' @param x an individual cell with multiple values
-#' @param sep a separator, a whitespace is the default
+#' @param x An individual cell with multiple values
+#' @param sep A separator, a whitespace is the default
 #'
-#' @return a vector of alphabetically sorted records
+#' @return A vector of alphabetically sorted records
 #'
 #' @examples \dontrun{util_separate_and_sort("z y x")}
 util_separate_and_sort <- function(x, sep = " ") {
 
-  # find cells with multiple values, indicated by presence of sep
+  # Find cells with multiple values, indicated by presence of sep
   i <- grep(sep, x)
-  # for those cells, split, sort then combine
+  # For those cells, split, sort then combine
   x[i] <- x[i] %>%
       stringr::str_split(" ") %>%
       lapply(function(xi) xi %>% sort() %>% paste(collapse = " ")) %>%
@@ -109,12 +109,12 @@ util_separate_and_sort <- function(x, sep = " ") {
 
 #'  Convert dataframe to list
 #'
-#'  Convert a dataframe to a named list, useful when converting to yaml
+#'  Convert a dataframe to a named list, useful when converting to yaml.
 #'
-#' @param df a dataframe
-#' @return a (yaml) list
+#' @param df A dataframe
+#' @return A (yaml) list
 #' @export
-#' @examples util_df_to_list(iris)
+#' @examples util_df_to_list(dplyr::starwars)
 util_df_to_list <- function(df) {
   attr(df, "out.attrs") <- NULL
   unname(lapply(split(df, seq_len(nrow(df))), as.list))
@@ -122,14 +122,14 @@ util_df_to_list <- function(df) {
 
 #' Convert a list of lists to dataframe
 #'
-#' Convert a list of lists to dataframe requires that every list have same named elements
+#' Convert a list of lists to dataframe; requires that every list have same named elements.
 #'
-#' @param my_list a list of lists to dataframe
-#' @param as_character logical:  indicating whether the values are read as character
-#' @param on_empty value to return if my_list is NULL, NA or is length == 0, default = NA
+#' @param my_list A list of lists to dataframe
+#' @param as_character A logical value, indicating whether the values are read as character
+#' @param on_empty Value to return if my_list is NULL, NA or is length == 0, default = NA
 #'
 #' @export
-#' @examples util_list_to_df2(util_df_to_list(iris))
+#' @examples util_list_to_df2(util_df_to_list(dplyr::starwars))
 util_list_to_df2 <- function(my_list, as_character = TRUE, on_empty = NA) {
 
   if (is.null(my_list) || any(is.na(my_list)) || length(my_list) == 0)
@@ -143,11 +143,11 @@ util_list_to_df2 <- function(my_list, as_character = TRUE, on_empty = NA) {
 
 #' Convert a list with single entries to dataframe
 #'
-#' @param my_list a list with single entries
-#' @return a tibble with two columns
+#' @param my_list A list with single entries
+#' @return A tibble with two columns
 #' @export
 #' @examples \dontrun{
-#' util_list_to_df1(as.list(iris)[2])
+#' util_list_to_df1(as.list(dplyr::starwars)[2])
 #' }
 util_list_to_df1 <- function(my_list) {
 
@@ -164,7 +164,7 @@ util_list_to_df1 <- function(my_list) {
 #'
 #' @param bib BibEntry object
 #'
-#' @return list
+#' @return List
 util_bib_to_list <- function(bib) {
 
   # Read in file, convert to list, set key
@@ -182,21 +182,21 @@ util_bib_to_list <- function(bib) {
 
 #' Add an item to the end of a list
 #'
-#' @param my_list a list
-#' @param to_append a list
+#' @param my_list A list
+#' @param to_append A list
 #'
-#' @return a list merged with an added item at the end
+#' @return A list merged with an added item at the end
 #' @examples  \dontrun{
-#' util_append_to_list(as.list(iris)[c(1,2)], as.list(iris)[c(3,4)])
+#' util_append_to_list(as.list(dplyr::starwars)[c(1,2)], as.list(dplyr::starwars)[c(3,4)])
 #' }
 util_append_to_list <- function(my_list, to_append) {
-  my_list[[length(my_list) + 1]] <-  to_append
+  my_list[[length(my_list) + 1]] <- to_append
   my_list
 }
 
-#' Read in a metadata.yml file for a study
+#' Read in a `metadata.yml` file for a study
 #'
-#' @param path location of the metadata file
+#' @param path Location of the metadata file
 #' @importFrom rlang .data
 #'
 #' @export
@@ -205,7 +205,8 @@ read_metadata <- function(path) {
   data <- yaml::read_yaml(path)
 
   # We want to preserve formatting in custom R code
-  # but read_yaml looses it. So read in as text, if not empty
+  # but `read_yaml` loses it
+  # So read in as text, if not empty
   if (!is.na(data$dataset$custom_R_code)) {
     # Read in again, extracting custom R code
 
@@ -236,13 +237,13 @@ read_metadata_dataset <- function(dataset_id) {
 }
 
 
-#' Write metadata.yml for a study
+#' Write `metadata.yml` for a study
 #'
-#' write metadata.yml file with custom R code formatted to allow line breaks
+#' Write `metadata.yml` file with custom R code formatted to allow line breaks.
 #'
-#' @param data austraits metadata object (a list)
-#' @param path location where the metadata file is to be written to
-#' @param style_code  should the R code be styled?
+#' @param data `austraits` metadata object (a list)
+#' @param path Location where the metadata file is to be written to
+#' @param style_code Should the R code be styled?
 #'
 #' @rdname write_metadata
 #' @importFrom rlang .data
@@ -283,13 +284,13 @@ write_metadata <- function(data, path, style_code = FALSE) {
 }
 
 
-#' Write the YAML representation of metadata.yml for specified `dataset_id` to
+#' Write the YAML representation of `metadata.yml` for specified `dataset_id` to
 #' file \code{data/dataset_id/metadata.yml}
 #'
 #' @inheritParams metadata_path_dataset_id
-#' @param metadata metadata file
+#' @param metadata Metadata file
 #'
-#' @return a yml file
+#' @return A yaml file
 write_metadata_dataset <- function(metadata, dataset_id) {
   write_metadata(metadata, dataset_id %>% metadata_path_dataset_id())
 }
@@ -300,13 +301,13 @@ write_metadata_dataset <- function(metadata, dataset_id) {
 #' `create_tree_branch()` is used to create a tree structure to show how things
 #' are related. In AusTraits, this is used in the vignettes to show the file
 #' structure of the repository and also to show the different components of the
-#' AusTraits database
+#' AusTraits database.
 #'
-#' @param x vector of terms
-#' @param title name of branch
-#' @param prefix specifies the amount of indentation
+#' @param x Vector of terms
+#' @param title Name of branch
+#' @param prefix Specifies the amount of indentation
 #'
-#' @return vector of character strings for the tree structure
+#' @return Vector of character strings for the tree structure
 create_tree_branch <- function(x, title, prefix = "") {
   c(
     sprintf("%s%s", prefix, title),
@@ -321,10 +322,10 @@ create_tree_branch <- function(x, title, prefix = "") {
 #' Strip scientific names of formatting and abbreviations
 #'
 #' Enables better fuzzy matching of scientific names by
-#' reducing the number of characters that could differ
+#' reducing the number of characters that could differ.
 #'
-#' @param x vector of names to clean
-#' @return vector of cleaned names
+#' @param x Vector of names to clean
+#' @return Vector of cleaned names
 #' @importFrom stringr fixed
 #' @export
 #' @examples c("Bankisa_serrata", "bankisa  serrata", "Banksia Seratta") %>% util_strip_taxon_names()
