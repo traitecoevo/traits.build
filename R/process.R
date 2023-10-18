27c4e782f75b859e6e2db593dd1b3a75a8d00d59
yangsophieee <sophie.yang917@gmail.com>
2023-10-18 22:19:26 +1100

    Add formatting changes

--- R/process.R
+++ R/process.R
@@ -1188,28 +1188,27 @@ process_convert_units <- function(data, definitions, unit_conversion_functions) 
   }
 
   f_range_bin <- function(value, name) {
-
-    stringr::str_split(value, "\\-\\-") %>%       # split into parts
-    purrr::map(f_standard, name) %>%                       # apply unit conversions to each
-    purrr::map_chr(~paste(.x, collapse = "--"))   # paste back together
+    stringr::str_split(value, "\\-\\-") %>%       # Split into parts
+    purrr::map(f_standard, name) %>%              # Apply unit conversions to each
+    purrr::map_chr(~paste(.x, collapse = "--"))   # Paste back together
   }
 
   # Split by unique unit conversions, to allow for as few calls as possible
   data <- data %>%
     dplyr::group_by(.data$ucn, .data$to_convert) %>%
     dplyr::mutate(
-      # standard conversion
-      value = ifelse(.data$to_convert == TRUE &                     # value requires conversion
-                      !.data$value_type %in% c("bin", "range") &    # value_type not a bin or range; those are converted below
-                      !is.na(.data$value),                          # value not NA - the full matrix from data.csv file is still in data table
-                      f_standard(.data$value, .data$ucn[1]),        # convert value to appropriate units
-                     .data$value),                                  # if conditions not met, keep original value
-      # value is a range of bin
-      value = ifelse(.data$to_convert == TRUE  &                    # value requires conversion
-                      .data$value_type %in% c("bin", "range") &     # value_type is a bin or range
-                      !is.na(.data$value),                          # value not NA - the full matrix from data.csv file is still in data table
-                      f_range_bin(.data$value, .data$ucn[1]),       # convert value to appropriate units
-                     .data$value),                                  # if conditions not met, keep original value
+      # Standard conversion
+      value = ifelse(.data$to_convert == TRUE &                     # Value requires conversion
+                      !.data$value_type %in% c("bin", "range") &    # `value_type` not a bin or range; those are converted below
+                      !is.na(.data$value),                          # Value not NA - the full matrix from data.csv file is still in data table
+                      f_standard(.data$value, .data$ucn[1]),        # Convert value to appropriate units
+                     .data$value),                                  # If conditions not met, keep original value
+      # Value is a range of bin
+      value = ifelse(.data$to_convert == TRUE  &                    # Value requires conversion
+                      .data$value_type %in% c("bin", "range") &     # `value_type` is a bin or range
+                      !is.na(.data$value),                          # Value not NA - the full matrix from data.csv file is still in data table
+                      f_range_bin(.data$value, .data$ucn[1]),       # Convert value to appropriate units
+                     .data$value),                                  # If conditions not met, keep original value
       unit = ifelse(.data$to_convert, .data$to, .data$unit_in)
     ) %>%
     dplyr::ungroup() %>%
