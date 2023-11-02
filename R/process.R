a79a9906268d46c2900ff3ca10593a3c2f5862cf
ehwenk <ehwenk@gmail.com>
2023-11-02 13:36:04 +1100

    filter taxonomic_updates to only include taxa for which trait data exists

--- R/process.R
+++ R/process.R
@@ -262,7 +262,8 @@ dataset_process <- function(filename_data_raw,
     dplyr::filter(!is.na(.data$error)) %>%
     dplyr::select(dplyr::all_of(c("error")), everything()) %>%
     dplyr::select(-dplyr::all_of(c("unit_in"))),
-    taxonomic_updates = taxonomic_updates,
+    taxonomic_updates = taxonomic_updates %>%
+      dplyr::filter(aligned_name %in% traits$taxon_name),
     taxa = taxonomic_updates %>%
       dplyr::select(dplyr::all_of(c(taxon_name = "aligned_name"))) %>%
       dplyr::distinct(),
