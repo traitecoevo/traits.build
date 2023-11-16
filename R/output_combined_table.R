a67a1b0197e7caef878d739c87f043f3d6edc08d
Elizabeth Wenk <ehwenk@gmail.com>
2023-11-16 15:09:03 +1100

    Update output_combined_table.R
    - add in contributors
    - ensure the symbols we are using for collapsing data aren't in the data itself

--- R/output_combined_table.R
+++ R/output_combined_table.R
@@ -11,7 +11,13 @@ database_create_combined_table <- function(database) {
   location_properties <- 
     database$locations %>%
       dplyr::filter(!location_property %in% c("latitude (deg)", "longitude (deg)")) %>%
-      dplyr::mutate(location_properties = paste0(location_property, "='", value,"'")) %>%
+      dplyr::mutate(
+        location_property = stringr::str_replace_all(location_property, "=", "-"),
+        value = stringr::str_replace_all(value, "=", "-"),
+        location_property = stringr::str_replace_all(location_property, ";", ","),
+        value = stringr::str_replace_all(value, ";", ",")
+      ) %>%
+      dplyr::mutate(location_properties = paste0(location_property, "=", value)) %>%
       dplyr::select(dplyr::all_of(c("dataset_id", "location_id", "location_name", "location_properties"))) %>%
       dplyr::group_by(dataset_id, location_id, location_name) %>%
       dplyr::mutate(location_properties = paste0(location_properties, collapse = "; ")) %>%
@@ -21,10 +27,16 @@ database_create_combined_table <- function(database) {
   contributors <-
     database$contributors %>%
       dplyr::mutate(
+        affiliation = stringr::str_replace_all(affiliation, ":", "-"),
+        affiliation = stringr::str_replace_all(affiliation, ";", ","),
+        affiliation = stringr::str_replace_all(affiliation, "<", "("),
+        affiliation = stringr::str_replace_all(affiliation, ">", ")"),
+        additional_role = stringr::str_replace_all(additional_role, "<", "("),
+        additional_role = stringr::str_replace_all(additional_role, ">", ")"),
         data_collectors = paste0(given_name, " ", last_name),
-        data_collectors = ifelse(!is.na(ORCID), paste0(data_collectors, " (ORCID:",ORCID), data_collectors),
-        data_collectors = ifelse(is.na(ORCID), paste0(data_collectors, " (affiliation:",affiliation), paste0(data_collectors, ";affiliation:",affiliation)),
-        data_collectors = ifelse(!is.na(additional_role), paste0(data_collectors, ";additional_role:",additional_role,")"), paste0(data_collectors,")"))
+        data_collectors = ifelse(!is.na(ORCID), paste0(data_collectors, " <ORCID:",ORCID), data_collectors),
+        data_collectors = ifelse(is.na(ORCID), paste0(data_collectors, " <affiliation:",affiliation), paste0(data_collectors, ";affiliation:",affiliation)),
+        data_collectors = ifelse(!is.na(additional_role), paste0(data_collectors, ";additional_role:",additional_role,">"), paste0(data_collectors,">"))
       ) %>%
       dplyr::select(-dplyr::all_of(c("last_name", "given_name", "ORCID", "affiliation", "additional_role"))) %>%
       dplyr::group_by(dataset_id) %>%
@@ -35,7 +47,13 @@ database_create_combined_table <- function(database) {
   contexts_tmp <- 
     database$contexts %>%
       dplyr::mutate(
-        value = ifelse(is.na(description), paste0(context_property, ":", value), paste0(context_property, ":", value, " (", description, ")"))
+        context_property = stringr::str_replace_all(context_property, "=", "-"),
+        value = stringr::str_replace_all(value, "=", "-"),
+        description = stringr::str_replace_all(description, "=", "-"),
+        context_property = stringr::str_replace_all(context_property, ";", ","),
+        value = stringr::str_replace_all(value, ";", ","),
+        description = stringr::str_replace_all(description, "=", "-"),
+        value = ifelse(is.na(description), paste0(context_property, ":", value), paste0(context_property, ":", value, " <", description, ">"))
       ) %>%
     dplyr::select(-dplyr::all_of(c("description", "context_property", "category"))) %>%
     tidyr::separate_longer_delim(link_vals, ", ") %>% distinct()
@@ -55,23 +73,23 @@ database_create_combined_table <- function(database) {
 
   join_contexts <- function(data, contexts_tmp) {
     data %>%
-    left_join(
+      dplyr::left_join(
       by = c("dataset_id", "treatment_context_id"),
       reformat_contexts(contexts_tmp, "treatment_context_id")
     ) %>%
-      left_join(
+      dplyr::left_join(
         by = c("dataset_id", "plot_context_id"),
         reformat_contexts(contexts_tmp, "plot_context_id")
       ) %>%
-      left_join(
+      dplyr::left_join(
         by = c("dataset_id", "entity_context_id"),
         reformat_contexts(contexts_tmp, "entity_context_id")
       ) %>%
-      left_join(
+      dplyr::left_join(
         by = c("dataset_id", "temporal_context_id"),
         reformat_contexts(contexts_tmp, "temporal_context_id")
       ) %>%
-      left_join(
+      dplyr::left_join(
         by = c("dataset_id", "method_context_id"),
         reformat_contexts(contexts_tmp, "method_context_id")
       )
@@ -79,12 +97,13 @@ database_create_combined_table <- function(database) {
 
   combined_table <-
     database$traits %>%
-      left_join(location_latlon, by = c("dataset_id", "location_id")) %>%
-      left_join(location_properties, by = c("dataset_id", "location_id", "location_name")) %>%
+      dplyr::left_join(location_latlon, by = c("dataset_id", "location_id")) %>%
+      dplyr::left_join(location_properties, by = c("dataset_id", "location_id", "location_name")) %>%
       join_contexts(contexts_tmp) %>%
-      left_join(database$methods %>% dplyr::select(-dplyr::all_of(c("data_collectors"))), by = c("dataset_id", "trait_name", "method_id")) %>%
-      left_join(database$taxa, by = c("taxon_name")) %>%
-      left_join(database$taxonomic_updates, by = c("taxon_name", "dataset_id", "original_name"))
+      dplyr::left_join(database$methods %>% dplyr::select(-dplyr::all_of(c("data_collectors"))), by = c("dataset_id", "trait_name", "method_id")) %>%
+      dplyr::left_join(contributors, by = c("dataset_id")) %>%
+      dplyr::left_join(database$taxa, by = c("taxon_name")) %>%
+      dplyr::left_join(database$taxonomic_updates, by = c("taxon_name", "dataset_id", "original_name"))
 
   combined_table
 }
