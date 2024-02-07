de0b7304d078847455fe6aa6913f13ddda93f7ac
Elizabeth Wenk <ehwenk@gmail.com>
2024-02-07 17:51:09 +1100

    Publish ontology (#148)
    Build a preliminary version of the traits.build ontology (data model), with terms registered at the w3id.org/traits.build namespace
    
        includes tables documenting the data model and all published terms reused within the data model
        includes scripts to build the tables into different RDF representations
        includes scripts to build the turtle file into a website, sorting terms into useful clusters

--- ontology/build_traits.build_ontology.R
+++ ontology/build_traits.build_ontology.R
@@ -1,9 +1,12 @@
 library(dplyr)
 library(rdflib)
 
-ontology_csv <- readr::read_csv("ontology/traits.build_ontology.csv")
-resource_triples <- readr::read_csv("ontology/traits.build_resource.csv")
-published_classes_csv <- readr::read_csv("ontology/traits.build_ontology_published_classes.csv")
+output_path <- "ontology/output/ontology"
+data_path <- "ontology/data"
+
+ontology_csv <- readr::read_csv(file.path(data_path, "traits.build_ontology.csv"))
+resource_triples <- readr::read_csv(file.path(data_path, "traits.build_resource.csv"))
+published_classes_csv <- readr::read_csv(file.path(data_path, "traits.build_ontology_published_classes.csv"))
 
 convert_to_triples <- function(ontology_csv, published_classes_csv) {
 
@@ -47,7 +50,7 @@ convert_to_triples <- function(ontology_csv, published_classes_csv) {
         "<http://www.w3.org/2004/02/skos/core#prefLabel>" = "prefLabel",
         "<http://www.w3.org/2004/02/skos/core#altLabel>" = "altLabel",
         "<http://www.w3.org/2004/02/skos/core#exactMatch>" = "exactMatch",
-        "<http://www.w3.org/2008/05/skos#broader>" = "broader",
+        "<http://www.w3.org/2004/02/skos/core#broader>" = "broader",
         "<http://semanticscience.org/resource/SIO_000602>" = "computational_entity",
         "<http://www.w3.org/2004/02/skos/core#closeMatch>" = "closeMatch",
         "<http://www.w3.org/2004/02/skos/core#relatedMatch>" = "relatedMatch",
@@ -74,7 +77,7 @@ convert_to_triples <- function(ontology_csv, published_classes_csv) {
         "<http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#hasMeasurement>" = "oboe-core:hasMeasurement",
         "<http://purl.org/dc/terms/description>" = "dcterms:description",
         "<http://www.w3.org/2000/01/rdf-schema#comment>" = "rdfs:comment",
-        "<http://www.w3.org/2000/01/rdf-schema#Datatype>" = "rdf:datatype",
+        "<http://www.w3.org/1999/02/22-rdf-syntax-ns#datatype>" = "rdf:datatype",
         "<http://purl.org/dc/terms/created>" = "dcterms:created"
       ))) %>%
     dplyr::mutate(
@@ -108,7 +111,7 @@ convert_to_triples <- function(ontology_csv, published_classes_csv) {
       Object = ifelse(stringr::str_detect(Object_tmp, "\\:") & is.na(Object),
                        paste0("<", published_classes_csv$Entity[match(Object_tmp, published_classes_csv$identifier2)],">"), 
                        Object),
-      Object = ifelse(stringr::str_detect(Predicate,"schema\\#Datatype"), 
+      Object = ifelse(stringr::str_detect(Predicate,"\\#datatype"), 
                        paste0("<", Object_tmp, ">"), 
                        Object),
       Object = ifelse(stringr::str_detect(Predicate,"inScheme"), 
@@ -116,7 +119,8 @@ convert_to_triples <- function(ontology_csv, published_classes_csv) {
                       Object),
       Object = ifelse(stringr::str_detect(Predicate,"created"), 
                       paste0("\"", Object_tmp, "\"", "^^<xsd:date>"),
-                      Object)
+                      Object),
+      Subject = ifelse(Subject == "<https://w3id.org/traits.build/traits.build>", "<https://w3id.org/traits.build>", Subject)
       ) %>%
     dplyr::select(-Object_tmp)
   
@@ -130,21 +134,61 @@ convert_to_triples <- function(ontology_csv, published_classes_csv) {
   triples
 }
 
-triples %>%
-  readr::write_csv("ontology/traits.build_triples.csv")
+triples <- convert_to_triples(ontology_csv, published_classes_csv)
 
 triples %>%
-  readr::write_delim("ontology/traits.build.nq", col_names=FALSE, escape="none", quote="none")
+  readr::write_delim(file.path(output_path, "traits.build.nq"), col_names=FALSE, escape="none", quote="none")
 
 triples %>%
   select(-graph) %>%
-  readr::write_delim("ontology/traits.build.nt", col_names = FALSE, escape = "none", quote = "none")
+  readr::write_delim(file.path(output_path, "traits.build.nt"), col_names = FALSE, escape = "none", quote = "none")
+
+triples <- triples %>%
+  dplyr::mutate(
+    predicate_tmp = stringr::str_replace(Predicate, "<", ""),
+    predicate_tmp = stringr::str_replace(predicate_tmp, ">", ""),
+    object_tmp = stringr::str_replace_all(Object, "<", ""),
+    object_tmp = stringr::str_replace_all(object_tmp, ">", ""),
+    object_tmp = stringr::str_replace(object_tmp, "\\^\\^", ""),
+    object_tmp = stringr::str_replace(object_tmp, "xsd:date", ""),
+    object_tmp = stringr::str_replace(object_tmp, "xsd:anyURI", ""),
+    object_tmp = stringr::str_replace(object_tmp, "xsd:Name", ""),
+    object_tmp = stringr::str_replace(object_tmp, "xsd:int", ""),
+    object_tmp = stringr::str_replace(object_tmp, "xsd:decimal", ""),
+    object_tmp = stringr::str_replace(object_tmp, "@en", ""),
+    object_tmp = stringr::str_replace(object_tmp, "[:punct:]$",""),
+    object_tmp = stringr::str_replace(object_tmp, "^[:punct:]",""),
+    subject_tmp = stringr::str_replace(Subject, "<https://w3id.org/traits.build/", ""),
+    subject_tmp = stringr::str_replace(subject_tmp, "<", ""),
+    subject_tmp = stringr::str_replace(subject_tmp, ">", "")
+  )
+
+labels <- triples %>%
+  filter(predicate_tmp %in% c("http://www.w3.org/2004/02/skos/core#prefLabel", "http://www.w3.org/2004/02/skos/core#altLabel")) %>%
+  mutate(
+    subject_tmp = stringr::str_replace(Subject, "<", ""),
+    subject_tmp = stringr::str_replace(subject_tmp, ">", "")
+  )
+
+triples <- triples %>%
+  mutate(
+    Predicate_labels = published_classes_csv$`skos:prefLabel`[match(predicate_tmp, published_classes_csv$Entity)],
+    Object_labels = ifelse(stringr::str_detect(object_tmp, "^http"),
+                           labels$object_tmp[match(object_tmp, labels$subject_tmp)],
+                           object_tmp),
+    Object_labels = ifelse(is.na(Object_labels), object_tmp, Object_labels),
+    Subject_labels = ontology_csv$prefLabel[match(subject_tmp, ontology_csv$Class)]
+  )
+  
+triples %>%
+  readr::write_csv(file.path(output_path, "build_triples.csv"))
+
 
 # prove this parses correctly
-true_triples <- read_nquads("ontology/traits.build.nq")
+true_triples <- read_nquads(file.path(output_path, "traits.build.nq"))
 
 # serialize to any format
-rdflib::rdf_serialize(true_triples, "ontology/traits.build.ttl",
+rdflib::rdf_serialize(true_triples, file.path(output_path, "traits.build.ttl"),
                       namespace = c(traits.build = "https://w3id.org/traits.build/",
                                     dc = "http://purl.org/dc/elements/1.1/",
                                     skos = "http://www.w3.org/2004/02/skos/core#",
@@ -156,10 +200,24 @@ rdflib::rdf_serialize(true_triples, "ontology/traits.build.ttl",
                                     oboe = "http://ecoinformatics.org/oboe/oboe.1.2/oboe-core.owl#",
                                     owl = "http://www.w3.org/2002/07/owl#",
                                     rdfs = "http://www.w3.org/2000/01/rdf-schema#",
+                                    rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
                                     datacite = "http://purl.org/datacite/v4.4/",
                                     SIO = "http://semanticscience.org/resource/",
                                     gdmt = "http://vocab.fairdatacollective.org/gdmt/")
 )
-rdflib::rdf_serialize(true_triples, "ontology/traits.build.json", format="jsonld")
+rdflib::rdf_serialize(true_triples, file.path(output_path, "traits.build.json"), format="jsonld")
 
-  
+
+# Put a copy of ontology in place found by pkgdown
+quarto::quarto_render("ontology/traits.build.qmd", output_format = "html")
+version <- rmarkdown::yaml_front_matter("ontology/traits.build.qmd")$params$version
+
+file.copy("ontology/traits.build.html", "ontology/output/ontology/traits.build.html", overwrite = TRUE)
+unlink("ontology/traits.build.html")
+
+# Copy into folder for specific release
+files <- c("traits.build.json", "traits.build.nq", "traits.build.nt", "traits.build.ttl", "traits.build.html")
+to_path <- file.path("ontology/output/ontology/release", version)
+dir.create(to_path, FALSE, TRUE)
+purrr::walk(files, ~ file.copy(file.path("ontology/output/ontology", .x), file.path(to_path, .x), overwrite = TRUE))
+
